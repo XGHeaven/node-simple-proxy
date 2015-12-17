a7df69d2b4f4e2073cad959fa298b791551af105
XGHeaven <619943612@qq.com>
2015-12-17 20:49:45 +0800

    test again

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let app = http.createServer((req, res) => {
 	let headers = Object.assign({}, req.headers)
 	delete headers['host'];
 
+	console.log(req.url)
 	let url = req.url.slice(1)
 	console.log(url)
 	if (url[0] === '/' && url[1] === '/') url = 'http:' + url
